85ca539d33e914fd235342e46f8813f81b224bfe
dhia <dhiaboumiza@gmail.com>
2022-01-04 11:01:00 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ManageTask from "./Components/ManageTask";
+
+function App() {
+	return (
+		<div className="App">
+			<ManageTask className="gyg"/>
+		</div>
+	);
+}
+
+export default App;
